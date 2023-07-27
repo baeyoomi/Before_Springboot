e8d98379509fe057a19ded07962354dc0c23c5d3
baeyoomi <cxz3930@gmail.com>
2023-07-27 21:38:42 +0900

    0727 commit

---
+++ quiz/test_quizboard_view_ver1/src/main/resources/static/js/scriptquiz.js
@@ -0,0 +1,15 @@
+﻿$(function(){
+ 	$('.table').hide();
+	$('.quizDown').css('display','block');
+	$('.quizDown').on('click',function(){
+		$('.table').stop(true, true).slideDown();
+		$('.quizDown').css('display','none');
+		$('.quizUp').css('display','block');
+	});
+	$('.quizUp').on('click',function(){
+		$('.table').stop(false, false).slideUp();
+		$('.quizUp').css('display','none');
+		$('.quizDown').css('display','block');
+	});	
+});
+
