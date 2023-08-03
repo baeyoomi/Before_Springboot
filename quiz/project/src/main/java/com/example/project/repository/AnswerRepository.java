beff764b6a08fa777d551237b20becf3603eab3b
baeyoomi <cxz3930@gmail.com>
2023-08-03 18:32:56 +0900

    0803 commit

--- quiz/project/src/main/java/com/example/project/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.Entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-}
--- quiz/project/src/main/java/com/example/project/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.Entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
-}
